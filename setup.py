060af925a7f8261424c5fa5008be727170e2297c
Sohail1173 <mdsohailahmed711@gmail.com>
2024-03-04 15:15:42 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'sign_detection',
+    version= '0.0.0',
+    author= 'Sohail',
+    author_email= 'mdsohailahmed711@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
